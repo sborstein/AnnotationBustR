04bff0c36f3d8f4e65a2d09fbb5b93740757af5a
Sam Borstein <sam@borstein.com>
2016-03-10 14:18:58 -0500

    Fixed function that uses ape to get around trait issues with cf, sp., trinomial etc.

--- findLongestSeq.R
+++ findLongestSeq.R
@@ -1,8 +1,15 @@
-#' Gets sequence lengths for a list of GenBank accession numbers and returns the GenBank accesion number with the longest sequence per species
-#' @param accessions A vector of GenBank accession numbers
+library(traits)
+library(ape)
+
+#' Find the longest sequence for each species from a list of GenBank accession numbers.
+#' @param accessions A vector of GenBank accession numbers.
+#' @return A list of genbank accessions numbers for the longest sequence for each taxon in a list of accession numbers.
 
 Find.Longest.Seq<-function(accessions){
       ncbi.hits<-ncbi_byid(accessions, format="fasta")[,c(1,3:5)]#use the ropensci traits package to read the accession numbers
+      get.names<-read.GenBank(accessions,species.name=TRUE)#use ape read genbank for getting around trinomial issue
+      Species = attr(get.names, "species")#get species from ape
+      ncbi.hits$taxon<-Species#sub the ape names in for the 
       unique.taxa<-unique(ncbi.hits$taxon)#get the names of taxa to find the longest seq
       final.accession<-NULL
       for(i in sequence(length(unique.taxa))){#for each taxa
@@ -11,8 +18,5 @@ Find.Longest.Seq<-function(accessions){
         final.accession<-rbind(final.accession,longest.seq)#combine individual rows into a data frame
       }
       row.names(final.accession)<-1:dim(final.accession)[1]#make row names the length of 
-      final.accession
+      final.accession#return final list
     }
-
-test<-Find.Longest.Seq(ncbi.numbers)
-
