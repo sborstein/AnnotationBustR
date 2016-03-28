e7159e9f702759a585d60da04000e075549e3851
Sam Borstein <sam@borstein.com>
2016-03-28 16:54:53 -0400

    Updated the getAnnotPos.R function by cleaning excess code up

--- R/getAnnotPos.R
+++ R/getAnnotPos.R
@@ -7,8 +7,6 @@ library(seqinr)
 #' @param bank Name of bank, either genbank or embl. Default is genbank.
 #' @return A table of start and stop positions of class annotPos for all the genes specified for all accession numbers that can be used to bust sequences using AnnotationBustR.
 
-genes<-read.csv("MitoGenesList.csv", header=TRUE)#test data
-#The function to find seq positions
 get.seq.pos<-function(accessions, genes, bank="genbank"){
 choosebank(bank)#choose bank so it could be genbank or EMBL or others supported?
 unique.gene.names <- unique(genes$gene)#unique gene names
