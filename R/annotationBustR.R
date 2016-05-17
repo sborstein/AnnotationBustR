d41bc7f25f1d2444fa41bac035bbf948966f3903
Sam Borstein <sam@borstein.com>
2016-05-17 16:30:40 -0400

    Changed the input name to be the same as for MakeAccessionTable. Also added a check to make sure input is of the right class.

--- R/annotationBustR.R
+++ R/annotationBustR.R
@@ -1,15 +1,18 @@
 #' Breaks up genbank sequences into their annotated components based on positions found using the get.seq.pos function.
-#' @param Seq.Positions Object of class Annot.Pos with the starting and stopping position for all genes.
+#' @param SeqPos Object of class Annot.Pos with the starting and stopping position for all genes.
 #' @return Returns a fasta file for each gene in the object of class Annot.Pos supplied into the function.
 #' @export
 
-AnnotationBust<-function(Seq.Positions){
-gene.starts <- which(grepl("start", colnames(Seq.Positions)))#get the start position for each sequence
+AnnotationBust<-function(SeqPos){
+if(length((class(SeqPos))) <2)#check to make sure class is length 2
+  stop("Input is not a data.frame or of class Annot.Pos")
+if(!(class(SeqPos)[2]=="Annot.Pos"))#if it is 2, check that it is Annot.Pos
+  stop("Input is not a data.frame or of class Annot.Pos")
+gene.starts <- which(grepl("start", colnames(SeqPos)))#get the start position for each sequence
 for (gene.index in sequence(length(gene.starts))) {
-  local.info <- data.frame(Species=Seq.Positions$Species, Accession=Seq.Positions$Accession, Start=Seq.Positions[,gene.starts[gene.index]], Stop=Seq.Positions[,1+gene.starts[gene.index]], stringsAsFactors=FALSE)#For each start, pul out the relevant info and next column which is the stop. Make it a non-factor.
+  local.info <- data.frame(Species=SeqPos$Species, Accession=SeqPos$Accession, Start=SeqPos[,gene.starts[gene.index]], Stop=SeqPos[,1+gene.starts[gene.index]], stringsAsFactors=FALSE)#For each start, pul out the relevant info and next column which is the stop. Make it a non-factor.
   local.info <- local.info[-which(is.na(local.info[,3])),]#For the current gene, remove entries from the column that are NA.
-  #gene.name <- gsub('.start', colnames(Seq.Positions)[gene.starts[gene.index]])
-  gene.name<-strsplit(colnames(Seq.Positions)[gene.starts[gene.index]],".",fixed=TRUE)[[1]][1]
+  gene.name<-strsplit(colnames(SeqPos)[gene.starts[gene.index]],".",fixed=TRUE)[[1]][1]
   open.command="w"
   for (taxon.index in sequence(dim(local.info)[1])) {
     current.seq<-ape::read.GenBank(local.info$Accession[taxon.index],species.names=TRUE,as.character=TRUE)#access number for read genbank
