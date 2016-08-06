569442d5e3b3ef04ca38fe001807b3e00e960247
Sam Borstein <sam@borstein.com>
2016-08-06 00:55:08 -0700

    fixed documentation issues preventing roxygenize from working and updated the namespace etc with roxygenize

--- R/annotationBustR.R
+++ R/annotationBustR.R
@@ -17,7 +17,7 @@
 #' @return Writes a fasta file(s) to the current working directory selected for each unique sub-sequence of interest in Terms containing all the accession numbers the sub-sequence was fond in
 #' @return Writes an data.frame of the accession numbers per loci that can be turned into an accession table using the function MakeAccessionTable
 #' @examples
-#' ncbi.accessions<-c("FJ706295",FJ706343","FJ706292")#vector of two NCBI accession numbers to get the annotation positions of.
+#' ncbi.accessions<-c("FJ706295","FJ706343","FJ706292")#vector of two NCBI accession numbers to get the annotation positions of.
 #' data(rDNA.Genes)#load rDNA search terms from AnnotationBustR
 #' my.sequences<-AnnotationBust(ncbi.accessions, rDNA.Genes, DuplicateSpecies=TRUE)#Run AnnotationBustR and write files to working directory
 #' my.sequences#Return the accession table for each species.
