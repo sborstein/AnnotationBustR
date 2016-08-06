26618943250a1976f9fd2b7126502fbec04b7c4f
Sam Borstein <sam@borstein.com>
2016-08-05 23:37:20 -0700

    new passing tests for MergeSearchTerms

--- tests/testthat/test_MergeSearchTerms.R
+++ tests/testthat/test_MergeSearchTerms.R
@@ -5,16 +5,17 @@ test_that("merging search terms works", {
 load("MitoGenes.RData")#load mito search terms
 load("rDNA.RData")#load rDNA search terms
 #No Sorting
-reg_merged<-rbind(Mito.Genes, rDNA.Genes)#simple rbind
-reg.test.merge<-MergeSearchTerms(Mito.Genes, rDNA.Genes, sort.gene = FALSE)#run the function w/out sorting
+reg_merged<-rbind(mtDNAterms, rDNAterms)#simple rbind
+reg.test.merge<-MergeSearchTerms(mtDNAterms, rDNAterms, SortGenes = FALSE)#run the function w/out sorting
 expect_identical(dim(reg.test.merge), dim(reg_merged))#test to see if same dimensions
 expect_identical(reg.test.merge[1,], reg_merged[1,])#test to see the first row is the same 
-expect_identical(reg.test.merge[167,], reg_merged[167,])#test to see the last row is the same 
+expect_identical(reg.test.merge[dim(reg.test.merge)[[1]],], reg_merged[dim(reg_merged)[[1]],])#test to see the last row is the same 
 #With Sorting
-sorted_merged<-reg_merged[order(reg_merged$gene),]
-sort.test.merge<-MergeSearchTerms(Mito.Genes, rDNA.Genes, sort.gene = TRUE)#run the function with sorting
+sorted_merged<-reg_merged[order(reg_merged$Locus),]#order the manual ones
+row.names(sorted_merged)<-1:dim(sorted_merged)[[1]]#re-number
+sort.test.merge<-MergeSearchTerms(mtDNAterms, rDNAterms, SortGenes = TRUE)#run the function with sorting
 expect_identical(dim(sort.test.merge), dim(sorted_merged))#test to see if same dimensions
 expect_identical(sort.test.merge[1,], sorted_merged[1,])#test to see the first row is the same 
-expect_identical(sort.test.merge[167,], sorted_merged[167,])#test to see the last row is the same
+expect_identical(sort.test.merge[dim(sort.test.merge)[[1]],], sorted_merged[dim(sorted_merged)[[1]],])#test to see the last row is the same
 })
 
