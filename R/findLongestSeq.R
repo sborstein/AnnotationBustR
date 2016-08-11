37ebadb2210ff5c0bdbd804085b993e87275e0fa
Sam Borstein <sam@borstein.com>
2016-08-11 00:51:02 -0700

    Fixed issues with taxize dependency and FindLongestSeq function. Uses ape only. Package no longer requires taxize.

--- R/findLongestSeq.R
+++ R/findLongestSeq.R
@@ -8,20 +8,17 @@
 #' @export
 
 FindLongestSeq<-function(accessions){
-  multi.ncbi.hits<-NULL#empty vector for merging records
-  final.accession<-NULL#empty vector
-  x<-seq_along(accessions)#seq along all to get number
-  split.access <- split(accessions, ceiling(x/600))#break them up into groups of 600
-  for (i in 1:length(split.access)){
-    ncbi.hits<-traits::ncbi_byid(split.access[[i]])[,c(1,3:5)]#use the ropensci traits package to read the accession numbers
-    multi.ncbi.hits<-rbind(multi.ncbi.hits,ncbi.hits)#merge together all search hits given by merging to the ith multiple of 600
+raw.accessions<-read.GenBank(accessions)#get the seqs
+seq.ids<-attr(raw.accessions, "species")#extract attribute-species name, from genbank takedown above
+seq.data<-data.frame(cbind(attr(raw.accessions, "species"), names(raw.accessions),as.vector(summary(raw.accessions)[,1])),stringsAsFactors = FALSE)#data frame it
+colnames(seq.data)<-c("Species","Accession","Length")#attribute column names
+seq.data$Length<-as.numeric(seq.data$Length)#make the length of the sequence numeric
+uni.taxa<-unique(seq.data$Species)#get unique taxa
+long.seqs<-data.frame(matrix(nrow=length(uni.taxa), ncol=dim(seq.data)[2]))#empty data frame to store results
+  for (taxa.index in 1:length(uni.taxa)){
+    current.tax<-subset(seq.data,seq.data$Species==uni.taxa[taxa.index])
+    longest.seq<-subset(current.tax,current.tax$Length==sort(as.numeric(current.tax$Length),decreasing = TRUE)[1])[1,]#id and grab longest seq
+    long.seqs[taxa.index, ]<-longest.seq#append data frame with longest seq
   }
-  unique.taxa<-unique(multi.ncbi.hits$taxon)#get the names of taxa to find the longest seq
-  for(j in sequence(length(unique.taxa))){#for each taxa
-    current.spec<-subset(multi.ncbi.hits,multi.ncbi.hits$taxon==unique.taxa[[j]])#get the data for all the records for species j, minus the sequence
-    longest.seq<-subset(current.spec,current.spec$length==sort(as.numeric(current.spec$length),decreasing = TRUE)[1])[1,]#for all of the
-    final.accession<-rbind(final.accession,longest.seq)#combine individual rows into a data frame
-  }
-  row.names(final.accession)<-1:dim(final.accession)[1]#make row names the length of
-  final.accession#return final list
+long.seqs#return longest seqs
 }
