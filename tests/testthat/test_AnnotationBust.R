a0897a3ff0ab881c911a7b0e4a200b28f1204ffd
Sam Borstein <sam@borstein.com>
2016-09-07 00:10:01 -0400

    changed unit tests to deal with new data objects

--- tests/testthat/test_AnnotationBust.R
+++ tests/testthat/test_AnnotationBust.R
@@ -3,10 +3,9 @@
 #Run with duplicate genes and translated seqs
 test_that("AnnotationBust Works",{
 data(mtDNAterms)
-setwd  ("C:/Users/Sam/Documents/GitHub/AnnotationBustR/Data")
-correct.out<-read.csv("AnnotationBustTest.csv", header = TRUE, stringsAsFactors = FALSE)#Manual output of all genes. Missing ND5 gene (misc_feature). Should be missing in real run.
+data(AnnotationBustOut)#Manual output of all genes. Missing ND5 gene (misc_feature). Should be missing in real run.
 #test with accession=JN628859.1
-setwd  ("C:/Users/Sam/Documents")
+setwd (tempdir())
 test.out<-AnnotationBust(Accessions = "JN628859.1", Terms = mtDNAterms, Duplicates = c("tRNA_Leu","tRNA_Ser"), DuplicateInstances = c(2,2), TranslateSeqs = TRUE, TranslateCode = 2)
-expect_identical(correct.out,test.out)
+expect_identical(AnnotationBustOut,test.out)
 })
