aee58917f3d21aaf28ef9746eab892bebaf1a78c
Sam Borstein <sam@borstein.com>
2016-09-07 11:25:40 -0400

    fixed issues with description and also trying sysdata for hidden data

--- tests/testthat/test_AnnotationBust.R
+++ tests/testthat/test_AnnotationBust.R
@@ -3,7 +3,7 @@
 #Run with duplicate genes and translated seqs
 test_that("AnnotationBust Works",{
 data(mtDNAterms)
-data(AnnotationBustOut)#Manual output of all genes. Missing ND5 gene (misc_feature). Should be missing in real run.
+data(sysdata)#Manual output of all genes. Missing ND5 gene (misc_feature). Should be missing in real run.
 #test with accession=JN628859.1
 setwd (tempdir())
 test.out<-AnnotationBust(Accessions = "JN628859.1", Terms = mtDNAterms, Duplicates = c("tRNA_Leu","tRNA_Ser"), DuplicateInstances = c(2,2), TranslateSeqs = TRUE, TranslateCode = 2)
