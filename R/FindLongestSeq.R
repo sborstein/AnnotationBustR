35b6e787afe550cdd93abf609e7fee4910380df5
Sam Borstein <sam@borstein.com>
2020-09-22 23:38:18 -0400

    fixed issue with database connection on CRAN. Now dontrun for database parts

--- R/FindLongestSeq.R
+++ R/FindLongestSeq.R
@@ -5,8 +5,10 @@
 #' @examples 
 #' #a vector of 4 genbank accessions, there are two for each species.
 #' genbank.accessions<-c("KP978059.1","KP978060.1","JX516105.1","JX516111.1")
+#' \dontrun{
 #' #returns the longest sequence respectively for the two species.
 #' long.seq.result<-FindLongestSeq(genbank.accessions)
+#' }
 #' @export
 
 FindLongestSeq<-function(Accessions){
