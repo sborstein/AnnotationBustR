43a303f9503d7dfda9e6093cdd34514eb6dda8dc
Sam Borstein <sam@borstein.com>
2026-06-16 13:33:34 -0500

    Update MergeSearchTerms given new data format

--- R/MergeSearchTerms.R
+++ R/MergeSearchTerms.R
@@ -10,34 +10,34 @@
 #' data(mtDNAterms) 
 #' 
 #' #Make a data.frame of new terms to add.
-#' #This is a dummy example for a non-real annoation of COI, but lets pretend it is real.
-#' add.name<-data.frame("COI","CDS", "CX1")
+#' #This is a dummy example for a non-real annotation of COI, but lets pretend it is real.
+#' add.name <- data.frame("COI","CDS", "CX1")
 #' 
 #' # make the column names the same for combination.
-#' colnames(add.name)<-colnames(mtDNAterms)
+#' colnames(add.name) <- colnames(mtDNAterms)
 #' 
 #' #Run the merge search term function without sorting based on gene name.
-#' new.terms<-MergeSearchTerms(add.name, mtDNAterms, SortGenes=FALSE)
+#' new.terms <- MergeSearchTerms(add.name, mtDNAterms, SortGenes=FALSE)
 #' 
 #' #Run the merge search term function with sorting based on gene name.
-#' new.terms<-MergeSearchTerms(add.name, mtDNAterms, SortGenes=TRUE)
+#' new.terms <- MergeSearchTerms(add.name, mtDNAterms, SortGenes=TRUE)
 #' 
 #' #Merge search terms and create an additional column for introns and/or exons to
 #' #In this example, add the trnK intron to the terms
 #' #create empty IntornExonNumber column for non-intron/exons
-#' cp.terms<-cbind(cpDNAterms,rep(NA,length(cpDNAterms$Name)))
-#' colnames(cp.terms)[4]<-"IntronExonNumber"#Name the column IntronExonNumber
-#' trnK.intron.terms<-subset(cpDNAterms,cpDNAterms$Locus=="trnK")#subset trnK
+#' cp.terms <- cbind(cpDNAterms,rep(NA,length(cpDNAterms$Name)))
+#' colnames(cp.terms)[4] <- "IntronExonNumber"#Name the column IntronExonNumber
+#' trnK.intron.terms <- subset(cpDNAterms,cpDNAterms$Feature=="trnK")#subset trnK
 #' #Create a vector of 1's the same length as the number of rows for trnK
-#' trnK.terms<-cbind(trnK.intron.terms,rep(1,length(trnK.intron.terms$Name)))
-#' colnames(trnK.terms)[4]<-"IntronExonNumber"#Name the column IntronExonNumber
+#' trnK.terms <- cbind(trnK.intron.terms,rep(1,length(trnK.intron.terms$Name)))
+#' colnames(trnK.terms)[4] <- "IntronExonNumber"#Name the column IntronExonNumber
 #' #Use MergeSearchTerms to merge the modified cpDNAterms and new intron terms
-#' all.terms<-MergeSearchTerms(cp.terms,trnK.terms)
+#' all.terms <- MergeSearchTerms(cp.terms,trnK.terms)
 #' @export
 
 MergeSearchTerms<-function(..., SortGenes=FALSE){
   dots <- list(...)
-  check.names<-c("Locus","Type","Name","IntronExonNumber")
+  check.names<-c("Feature","Type","Name","IntronExonNumber")
   if(length(unique(sapply(lapply(dots, dim), "[", 2)))>1){
     stop(paste("The input data frames are of different dimensions"))
   }
@@ -52,13 +52,13 @@ MergeSearchTerms<-function(..., SortGenes=FALSE){
       #working <- FALSE
     }
     if(length(grep(TRUE,colnames(dots[[i]])!=check.names[1:unique(sapply(lapply(dots, dim), "[", 2))])>0)) {
-      stop(paste("The columns for table",i, "do not have the proper names ('Locus','Type','Name' and if extracting introns/exons, the additional column 'IntronExonNumber')", sep=" "))
+      stop(paste("The columns for table",i, "do not have the proper names ('Feature','Type','Name' and if extracting introns/exons, the additional column 'IntronExonNumber')", sep=" "))
       #working <- FALSE
     }
   }
   new.terms <- rbind(...)
   if(SortGenes==TRUE) {
-    new.terms <- new.terms[order(new.terms$Locus),]
+    new.terms <- new.terms[order(new.terms$Feature),]
   }
   row.names(new.terms)<-1:dim(new.terms)[[1]]
   return(new.terms)
